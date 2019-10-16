ddd1d6c2fc1801453a0e678ca3937c099feee977
kwartler <ehk116@gmail.com>
2019-10-16 15:12:43 -0400

    add lesson for review

---
+++ Lessons/G_TimeSeries_Equities/scripts/TTR/TTR_B.R
@@ -0,0 +1,35 @@
+#' Author: Ted Kwartler
+#' Date: 9-2-2018
+#' Purpose: Simple Moving Avg Example
+#'
+
+# Opts
+options(scipen=999)
+
+# Libs
+library(TTR)
+library(quantmod)
+
+# Get Chipotle
+getSymbols("CMG") #"CMG_1_TTR_B.rds"
+CMG <- CMG['2018-03-01/']
+
+# Plot
+plot(CMG$CMG.Close)
+
+# Calculate the moving Avgs
+CMGma3  <- SMA(CMG$CMG.Close, 3)
+CMGma10 <- SMA(CMG$CMG.Close, 10)
+CMGma30 <- SMA(CMG$CMG.Close, 30)
+
+# Plot different MA windows
+plot(CMG$CMG.Close)
+lines(CMGma3, col='red')
+
+plot(CMG$CMG.Close)
+lines(CMGma10, col='red')
+
+plot(CMG$CMG.Close)
+lines(CMGma30, col='red')
+
+# End
---
+++ Lessons/G_TimeSeries_Equities/scripts/forecasting/0_amtrak.R
@@ -0,0 +1,65 @@
+#' Author: Ted Kwartler
+#' Date: 6-18-2018
+#' Purpose: Forecasting Basics
+#'
+
+# WD
+setwd("C:/Users/Edward/Desktop/HarvardFallAdmin2018/book material/datasets")
+
+# libs
+library(dygraphs)
+
+# Data
+df <- read.csv('Amtrak.csv')
+
+# Examine
+head(df, 10)
+class(df$Month)
+
+# Create a time series obj
+riders <- ts(df$Ridership, 
+             start = c(1991, 1), 
+             end = c(2004, 3), 
+             frequency = 12)
+
+riders
+class(riders)
+
+# Level
+riderLev <- mean(df$Ridership)
+
+# Basic Viz
+plot(riders, xlab = 'Months', ylab = "monthly Ridership in 000s")
+abline(a = riderLev, b = 0, col='red')
+
+# Dynamic to see cyclicality
+dygraph(riders, main = "Amtrack Ridership") %>% 
+  dyRangeSelector()
+
+# Bonus: Dealing with Dates
+library(lubridate)
+
+# Make a date obj
+cleanDate <- dmy(df$Month)
+class(cleanDate)
+
+# Extract as standalone data vecs
+df$day <- day(cleanDate)
+df$month <- month(cleanDate)
+df$yr <- year(cleanDate)
+
+head(df)
+
+# Calculate date differences w/difftime()
+Sys.Date()
+cleanDate[1]
+difftime(Sys.Date(), cleanDate[1])
+
+# Nest difftime to engineer new data vectors
+df$daysPassed <- as.numeric(difftime(Sys.Date(), cleanDate))
+df$monthsPassed <- as.numeric(difftime(Sys.Date(), cleanDate, units="weeks")) /4
+df$yrsPassed <- as.numeric(difftime(Sys.Date(), cleanDate, unit="weeks"))/52.25
+head(df)
+
+
+# End
