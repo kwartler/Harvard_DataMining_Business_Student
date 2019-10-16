b684eb87b68003164510fe29e0eebf11584e93bd
kwartler <ehk116@gmail.com>
2019-10-16 17:51:19 -0400

    add announcement

---
+++ Lessons/G_TimeSeries_Equities/class announcements.R
@@ -0,0 +1,6 @@
+#' Syllabus Due Date Changes & Review
+#' * HW 10: Nov. 21 (Thu) <= original plan (no change)
+#' * Case 2: Dec. 9 (Mon) <= changed deadline
+#' * HW 11 (final reflection only): Dec. 12 (Thu) <= changed deadline
+#' * Final Exam: Sunday, Dec. 15, 3:00 am (EST) to Tuesday, Dec. 17, 3:00 am (EST) <= finalized schedule
+#' * Writing Assignment: Dec. 19 (Thu) <= original plan (no change)
