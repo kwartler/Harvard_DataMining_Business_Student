f40fccb257bb8f2a80d880fda672cdc046cf8cf1
Edward Kwartler <ehk116@gmail.com>
2020-10-26 13:12:10 -0400

    add lesson

---
+++ Lessons/G_RF_TimeSeries/scripts/A_Bank Loans RandomForest_REVISED.R
@@ -0,0 +1,115 @@
+#' Author: Ted Kwartler
+#' Data: Oct 26
+#' Purpose: Load data build a random forest tree; this version uses more equally balanced target classes
+#' https://archive.ics.uci.edu/ml/datasets/bank+marketing
+
+
+## Set the working directory
+setwd("~/Documents/Harvard_DataMining_Business_Student/Lessons/G_RF_TimeSeries/data")
+
+# Options
+options(scipen=999)
+
+## Load the libraries
+library(MLmetrics)
+library(caret)
+library(rpart.plot) 
+library(randomForest)
+library(vtreat)
+
+## Bring in some data
+dat <- read.csv('bank-downSampled.csv')
+
+# EDA
+summary(dat)
+
+# To save time in class, we are only training on 20% of the data
+splitPercent <- round(nrow(dat) %*% .2)
+totalRecords <- 1:nrow(dat)
+set.seed(1234)
+idx <- sample(totalRecords, splitPercent)
+
+trainDat <- dat[idx,]
+testDat  <- dat[-idx,]
+
+# Treatment
+targetVar       <- names(trainDat)[17]
+informativeVars <- names(trainDat)[1:16]
+
+# Design a "C"ategorical variable plan 
+plan <- designTreatmentsC(trainDat, 
+                          informativeVars,
+                          targetVar,'yes')
+
+treatedTrain <- prepare(plan, trainDat)
+treatedTest  <- prepare(plan, testDat)
+
+# Fit a random forest model with Caret
+downSampleFit <- train(Class ~ .,
+                      data = treatedTrain,
+                      method = "rf",
+                      verbose = FALSE,
+                      ntree = 3,
+                      tuneGrid = data.frame(mtry = 1))
+downSampleFit
+
+predProbs   <- predict(downSampleFit,  
+                       treatedTrain, 
+                       type = c("prob"))
+predClasses <- predict(downSampleFit,  treatedTrain)
+
+# Confusion Matrix; MLmetrics has the same function but use CARET!!
+caret::confusionMatrix(predClasses, treatedTrain$Class)
+
+# Other interesting model artifacts
+varImp(downSampleFit)
+plot(varImp(downSampleFit), top = 20)
+
+# Add more trees to the forest with the randomForest package (caret takes a long time bc its more thorough)
+moreVoters <- randomForest(Class ~ .,
+                           data  = treatedTrain, 
+                           ntree = 500,
+                           mtry  = 1)
+
+# Confusion Matrix, compare to 3 trees ~63% accuracy
+trainClass <- predict(moreVoters, treatedTrain)
+confusionMatrix(trainClass, treatedTrain$Class)
+
+# Look at improved var importance
+varImpPlot(moreVoters)
+
+# Out of Bag OOB= avg prediction error on each training sample using trees that weren't built with those records (similar to a validation)
+#https://en.wikipedia.org/wiki/Out-of-bag_error
+
+# plot the RF with a legend
+# https://stackoverflow.com/questions/20328452/legend-for-random-forest-plot-in-r
+layout(matrix(c(1,2),nrow=1),
+       width=c(4,1)) 
+par(mar=c(5,4,4,0)) #No margin on the right side
+plot(moreVoters, log="y")
+par(mar=c(5,0,4,2)) #No margin on the left side
+plot(c(0,1),type="n", axes=F, xlab="", ylab="")
+legend("top", colnames(moreVoters$err.rate),col=1:4,cex=0.8,fill=1:4)
+
+
+# Let's optimize # of trees 
+someVoters <- randomForest(Class ~ .,
+                           data = treatedTrain, 
+                           ntree=100,
+                           mtyr = 1)
+
+# Confusion Matrix
+trainClass <- predict(someVoters, treatedTrain)
+confusionMatrix(trainClass, treatedTrain$Class)
+
+### Now let's apply to the validation test set
+threeVotes        <- predict(downSampleFit, treatedTest)
+fiveHundredVoters <- predict(moreVoters,    treatedTest)
+oneHundredVoters  <- predict(someVoters,    treatedTest)
+
+# Accuracy Comparison from MLmetrics
+Accuracy(testDat$Class, threeVotes)
+Accuracy(testDat$Class, fiveHundredVoters)
+Accuracy(testDat$Class, oneHundredVoters)
+
+# End
---
+++ Lessons/G_RF_TimeSeries/scripts/C_getRevenueData.R
@@ -0,0 +1,54 @@
+#' Author: Ted Kwartler
+#' Data: 3-23-2020
+#' Purpose: Grab some time series revenue data and visualize it
+#' 
+
+# Options
+options(scipen=999)
+
+# library
+library(jsonlite)
+library(lubridate)
+library(xts)
+library(ggplot2)
+
+# Choose a single stock ticker
+stock <-'AMZN' #CVS 
+
+# Open a browser with this URL to see the chart of data
+(paste0('https://ycharts.com/companies/',stock,'/revenues'))
+
+# Get the raw data under the chart
+(stockURL <- paste0('https://ycharts.com/charts/fund_data.json?securities=include%3Atrue%2Cid%3A',stock,'%2C%2C&calcs=include%3Atrue%2Cid%3Arevenues%2C%2C'))
+
+# Send R to go get it
+stockQtrRev <- fromJSON(stockURL)
+
+# Review
+(rawData <- stockQtrRev$chart_data[[1]]$raw_data)
+
+# Easier data type
+qtrDF <- data.frame(unixTime = rawData[[1]][,1],
+                    revBill  = rawData[[1]][,2])
+
+head(qtrDF)
+
+# Change to date
+qtrDF$date  <- as.POSIXct(qtrDF$unixTime/1000, origin = '1970-1-1')
+tail(qtrDF)
+
+# Change to a time series
+stYr  <- year(qtrDF$date[1])
+stQtr <- quarter(qtrDF$date[1])
+st    <- c(stYr, stQtr)
+
+# Base R time series
+qtrTS2 <- ts(qtrDF$revBill, start = st, frequency = 4)
+
+# Basic ggplot2
+p <- ggplot(qtrDF, aes(x=date, y=revBill)) +
+  geom_line() + 
+  xlab("")
+p
+
+# End
