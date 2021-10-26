92f5b11007e22c072b8180483c37a98d0366ea05
Edward Kwartler <edwardkwartler@Edwards-MacBook-Air.local>
2021-10-25 22:52:19 -0400

    g catch up lesson script

--- Lessons/G_timeSeries_CatchUp/scripts/F_HoltWintersWMT.R
+++ Lessons/G_timeSeries_CatchUp/scripts/F_HoltWintersWMT.R
@@ -6,7 +6,7 @@
 options(scipen=999)
 
 # Wd
-setwd("~/Desktop/Harvard_DataMining_Business_Student/Lessons/G_RF_TimeSeries/data")
+setwd("~/Desktop/Harvard_DataMining_Business_Student/Lessons/G_timeSeries_CatchUp/data")
 
 # library
 library(forecast)
