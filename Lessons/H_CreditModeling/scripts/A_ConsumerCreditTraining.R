2194996e7da8bb0f468bd826150c1d0ee668f372
Edward Kwartler <edwardkwartler@Edwards-MacBook-Air.local>
2022-10-31 17:59:43 -0400

    adjust two scripts

--- Lessons/H_CreditModeling/scripts/A_ConsumerCreditTraining.R
+++ Lessons/H_CreditModeling/scripts/A_ConsumerCreditTraining.R
@@ -1,5 +1,5 @@
 #' Author: Ted Kwartler
-#' Date: Apr 4 2022
+#' Date: Oct 31, 2022
 #' Purpose: Lending Club Modeling, Cross Validation
 #' 
 
@@ -16,45 +16,44 @@ library(dplyr)
 library(caret)
 library(e1071)
 library(vtreat)
+library(readr)
 
 # I/O
-df <- read.csv('20K_sampleLoans.csv') 
+df <- read_csv('https://raw.githubusercontent.com/kwartler/Harvard_DataMining_Business_Student/master/Lessons/H_CreditModeling/data/20K_sampleLoans.csv') 
+df <- as.data.frame(df)
 
 # Keep the pertinent information
 keeps <- c("loan_amnt", "term", "int_rate", "installment", "grade", "sub_grade", "emp_length" , "home_ownership", "annual_inc", "purpose", "title", "zip_code", "addr_state", "dti", "delinq_2yrs", "pub_rec_bankruptcies", "inq_last_6mths", "mths_since_last_delinq", "mths_since_last_record", "open_acc", "pub_rec", "revol_bal", "revol_util", "total_acc", "initial_list_status", "collections_12_mths_ex_med", "mths_since_last_major_derog","y")
 df    <- df[,keeps]
 
-## Sample
-set.seed(2022)
-idx        <- sample(1:nrow(df), 16000)
-training   <- df[idx,]
-validation <- df[-idx,]
+# Make a data change 
+df$revol_util <- as.numeric(gsub('%', '', df$revol_util))
+df$int_rate <- as.numeric(gsub('%', '', df$int_rate))
 
-## Explore
-head(training)
+## Sample & Segment the prep data
+set.seed(1234)
+idx         <- sample(1:nrow(df),.1*nrow(df))
+prepData    <- df[idx,]
+nonPrepData <- df[-idx,]
 
 ## Modify
-#Make % a numeric
-training$revol_util <- as.numeric(gsub('%', '', training$revol_util))
-training$int_rate   <- as.numeric(gsub('%', '', training$int_rate))
-validation$revol_util <- as.numeric(gsub('%', '', validation$revol_util))
-validation$int_rate   <- as.numeric(gsub('%', '', validation$int_rate))
-
-# Now easy variable treatment plan
-dataPlan <- designTreatmentsC(dframe        = training, 
+# Design a "C"ategorical variable plan 
+dataPlan <- designTreatmentsC(dframe        = prepData, 
                               varlist       = keeps,
                               outcomename   = 'y', 
                               outcometarget = 1)
-#saveRDS(dataPlan, 'consumerCredit_dataPlan.rds') #backup bc take a long time
 
-# Now apply the plan to the data
-treatedTrain <- prepare(dataPlan, training)
-treatedValid <- prepare(dataPlan, validation)
+# Apply to xVars
+treatedX <- prepare(dataPlan, nonPrepData)
 
-# Observe the difference
-ncol(training)
-ncol(treatedTrain)
-names(treatedTrain)
+# Partition to avoid over fitting
+set.seed(2022)
+idx        <- sample(1:nrow(treatedX),.8*nrow(treatedX))
+training   <- treatedX[idx,]
+validation <- treatedX[-idx,]
+
+## Explore
+head(training)
 
 ## Model
 # Now let's do a logistic regression with a 3 fold CV
@@ -64,45 +63,46 @@ crtl <- trainControl(method      = "cv",
 
 # Fit lm model using 3-fold CV: model
 fit3 <- train(as.factor(y) ~ ., 
-              data      = treatedTrain,
+              data      = training,
               method    = "glm",
               family    = "binomial",
               trControl = crtl)
 fit3
 
 ## Assess in sample
-preds <- predict(fit3, treatedTrain)
-table(preds, treatedTrain$y)
-MLmetrics::Accuracy(preds, treatedTrain$y)
+preds <- predict(fit3, training)
+table(preds, training$y)
+MLmetrics::Accuracy(preds, training$y)
 
 # what happens when we put in original data?
 newPreds <- predict(fit3, df)
 
 # Remember to use the treated version of the data
-newPreds <- predict(fit3, treatedValid)
-table(newPreds, treatedValid$y)
-MLmetrics::Accuracy(newPreds, treatedValid$y)
+newPreds <- predict(fit3, validation)
+table(newPreds, validation$y)
+MLmetrics::Accuracy(newPreds, validation$y)
 
 # Let's explore for our use case because equal accuracy isn't appropriate
 # predicted=1 but actual=0 is where we lose money
 # Organize
-losingMoney <- data.frame( probs   = predict(fit3, treatedValid, type = 'prob'),
-                           actuals = treatedValid$y)
+losingMoney <- data.frame( probs   = predict(fit3, validation, type = 'prob'),
+                           actuals = validation$y)
 head(losingMoney) 
 
 # Subset to preds =1, and actual = 0
 chk <- subset(losingMoney, losingMoney$probs.1>=0.5 & actuals == 0)
 nrow(chk)
-table(newPreds, treatedValid$y)
+table(newPreds, validation$y)
 
 # What is the probability distribution of these misclassifications (false positives)
 hist(chk$probs.1)
+plot(density(chk$probs.1))
 summary(chk$probs.1)
 
 # Depending on the amount of your portfolio and what we learned you may want to increase the cutoff to optimize payoff not overall accuracy
-losingMoney$highCutoff <- ifelse(losingMoney$probs.1 > 0.999,1,0)
-highThreshold <- table(losingMoney$highCutoff, treatedValid$y) #704 is now 55, much less chance of getting it wrong
+losingMoney$highCutoff <- ifelse(losingMoney$probs.1 > 0.98,1,0)
+highThreshold <- table(losingMoney$highCutoff, validation$y) #704 is now 55, much less chance of getting it wrong
 highThreshold
-MLmetrics::Accuracy(losingMoney$highCutoff, treatedValid$y)
+MLmetrics::Accuracy(losingMoney$highCutoff, validation$y)
 
 # End
--- Lessons/H_CreditModeling/scripts/B_ConsumerCreditScoring.R
+++ Lessons/H_CreditModeling/scripts/B_ConsumerCreditScoring.R
@@ -1,5 +1,5 @@
 #' Author: Ted Kwartler
-#' Date: April 4, 2022
+#' Date: Oct 31, 2022
 #' Purpose: Lending Club score notes and visualize
 #' 
 
@@ -16,49 +16,52 @@ library(dplyr)
 library(caret)
 library(e1071)
 library(vtreat)
+library(readr)
+library(MLmetrics)
+library(rbokeh)
 
 # I/O
-df <- read.csv('20K_sampleLoans.csv') 
-newNotes <- read.csv('OpenNotesJune18_v2.csv')
+df <- read_csv('https://raw.githubusercontent.com/kwartler/Harvard_DataMining_Business_Student/master/Lessons/H_CreditModeling/data/20K_sampleLoans.csv') 
+df <- as.data.frame(df)
+newNotes <- read_csv('https://raw.githubusercontent.com/kwartler/Harvard_DataMining_Business_Student/master/Lessons/H_CreditModeling/data/OpenNotesJune18_v2.csv')
 
 # Keep the pertinent information
-keeps <- c("loan_amnt", "term", "int_rate", "installment", "grade", "sub_grade", "emp_length" , "home_ownership", "annual_inc", "purpose", "title", "zip_code", "addr_state", "dti", "delinq_2yrs", "pub_rec_bankruptcies", "inq_last_6mths", "mths_since_last_delinq", "mths_since_last_record", "open_acc", "pub_rec", "revol_bal", "revol_util", "total_acc", "initial_list_status", "collections_12_mths_ex_med", "mths_since_last_major_derog","y")
+keeps <- c("loan_amnt", "term", "int_rate", "installment", "grade", "sub_grade", "emp_length" , "home_ownership", "annual_inc", "purpose", "title", "zip_code", "addr_state", "dti", "delinq_2yrs", "pub_rec_bankruptcies", "inq_last_6mths", "mths_since_last_delinq", "mths_since_last_record", "open_acc", "pub_rec", "revol_bal", "revol_util", "total_acc", "collections_12_mths_ex_med", "mths_since_last_major_derog","y")
 df    <- df[,keeps]
 
-## Sample
-set.seed(2022)
-idx        <- sample(1:nrow(df), 18000) #now we feel more comfortable with our model so we increase traning set
-training   <- df[idx,]
-validation <- df[-idx,]
+# Make a data change 
+df$revol_util <- as.numeric(gsub('%', '', df$revol_util))
+df$int_rate   <- as.numeric(gsub('%', '', df$int_rate))
+df$term       <- as.numeric(gsub(' months','',df$term))
+newNotes$revol_bal <- as.numeric(gsub('%', '', newNotes$revol_bal))
+newNotes$int_rate  <- as.numeric(gsub('%', '', newNotes$int_rate))
+newNotes$term      <- as.numeric(gsub(' months','',newNotes$term))
+newNotes$mths_since_last_major_derog <- NA
 
-## Explore
-head(training)
+## Sample & Segment the prep data
+set.seed(1234)
+idx         <- sample(1:nrow(df),.1*nrow(df))
+prepData    <- df[idx,]
+nonPrepData <- df[-idx,]
 
 ## Modify
-#Make % a numeric
-training$revol_util <- as.numeric(gsub('%', '', training$revol_util))
-training$int_rate   <- as.numeric(gsub('%', '', training$int_rate))
-training$term       <- as.numeric(gsub(' months','',training$term))
-
-validation$revol_util <- as.numeric(gsub('%', '', validation$revol_util))
-validation$int_rate   <- as.numeric(gsub('%', '', validation$int_rate))
-validation$term       <- as.numeric(gsub(' months','',validation$term))
-
-newNotes$revol_util <- as.numeric(gsub('%', '', newNotes$revol_util))
-newNotes$int_rate   <- as.numeric(gsub('%', '', newNotes$int_rate))
-newNotes$mths_since_last_major_derog <- as.numeric(newNotes$mths_since_last_major_derog)
-
-
-# Now easy variable treatment plan
-dataPlan <- designTreatmentsC(dframe        = training, 
+# Design a "C"ategorical variable plan 
+dataPlan <- designTreatmentsC(dframe        = prepData, 
                               varlist       = keeps,
                               outcomename   = 'y', 
                               outcometarget = 1)
 
-# Now apply the plan to the data
-treatedTrain <- prepare(dataPlan, training)
-treatedValid <- prepare(dataPlan, validation)
-treatedNew   <- prepare(dataPlan, newNotes)
+# Apply to xVars
+treatedX <- prepare(dataPlan, nonPrepData)
+
+# Partition to avoid over fitting
+set.seed(2022)
+idx        <- sample(1:nrow(treatedX),.8*nrow(treatedX))
+training   <- treatedX[idx,]
+validation <- treatedX[-idx,]
+
+## Explore
+head(training)
 
 # Model w/increased CV
 crtl <- trainControl(method      = "cv", 
@@ -67,14 +70,15 @@ crtl <- trainControl(method      = "cv",
 
 # Fit lm model using 10-fold CV: model
 finalFit <- train(as.factor(y) ~ ., 
-                  data      = treatedTrain, 
+                  data      = training, 
                   method    = "glm", 
                   family    = "binomial",
                   trControl = crtl)
 
 # Make predictions for 3 partitions
-trainProbs    <- predict(finalFit, treatedTrain, type = 'prob')
-testProbs     <- predict(finalFit, treatedValid, type = 'prob')
+trainProbs    <- predict(finalFit, training, type = 'prob')
+testProbs     <- predict(finalFit, validation, type = 'prob')
+treatedNew    <- prepare(dataPlan, newNotes) #remember the new notes need to be treated for the model!
 newNotesProbs <- predict(finalFit, treatedNew, type = 'prob')
 
 # Change the cutoff threshold
@@ -84,16 +88,16 @@ cutoffProbsTest     <- ifelse(testProbs[,2]>=cutoff, 1, 0)
 cutoffProbsNewNotes <- ifelse(newNotesProbs[,2]>=cutoff, 1, 0) 
 
 # Accuracy
-Accuracy(treatedTrain$y, cutoffProbsTrain)
-Accuracy(treatedValid$y, cutoffProbsTest)
+Accuracy(training$y, cutoffProbsTrain)
+Accuracy(validation$y, cutoffProbsTest)
 
-table(treatedTrain$y, cutoffProbsTrain)
-table(treatedValid$y, cutoffProbsTest)
+table(training$y, cutoffProbsTrain)
+table(validation$y, cutoffProbsTest)
 
 ## Suppose you want to review "A" and low risk notes 20% chance of default
 # Organize
 testSetComparison <- data.frame(y     = validation$y, #actual outcome
-                                grade = validation$grade, #lending club guess
+                                grade = nonPrepData[-idx,]$grade, #lending club guess
                                 risk  = testProbs[,1]) #probability of 0 in model
 head(testSetComparison)
 
@@ -103,7 +107,7 @@ onlyA <- subset(testSetComparison, testSetComparison$grade == "A")
 # Get their best guess with our best risk score
 testSetBEST <- subset(testSetComparison, 
                       testSetComparison$grade == "A" & 
-                        testSetComparison$risk <= 0.2 )
+                      testSetComparison$risk <= 0.1 )
 
 # How many were 0 in the ENTIRE test set? Random Selection 
 sum(testSetComparison$y==0) / nrow(testSetComparison)
@@ -111,10 +115,10 @@ sum(testSetComparison$y==0) / nrow(testSetComparison)
 # How many were 0 among "A" (their best guess)? Accept their model
 sum(onlyA$y==0)/nrow(onlyA)
 
-# How many were "A" and 20% default risk for comparison (our best guess)? Combine their expertise & our model
+# How many were "A" (their guess) &  10% default risk for comparison (our guess)? Combine their expertise & our model
 sum(testSetBEST$y==0) / nrow(testSetBEST)
 
-# Using "A" & "20%" now apply to open/new loans
+# Using "A" & "10%" now apply to open/new loans
 scoredNotes <- data.frame(id           = 1:nrow(treatedNew),
                           risk         = newNotesProbs[,1],
                           successProb  = newNotesProbs[,2],
@@ -125,9 +129,9 @@ scoredNotes <- data.frame(id           = 1:nrow(treatedNew),
 scoredNotes <- scoredNotes[order(scoredNotes$risk),]
 head(scoredNotes, 10)
 
-# Subset to "A" & 20%; our and their best guess
+# Subset to "A" & 10%; our and their best guess
 bestNotes <- subset(scoredNotes, 
-                    scoredNotes$LCgrade == "A" & scoredNotes$risk <= 0.2)
+                    scoredNotes$LCgrade == "A" & scoredNotes$risk <= 0.1)
 
 # Make a mkt Plot
 mktPlot <- figure(legend_location = "bottom_right") %>% 
